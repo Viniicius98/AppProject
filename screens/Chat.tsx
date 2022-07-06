027d65d88dc5bf4f0d17404734e86c55f5ffdf73
Evandro Araujo <64655153+Evandromck@users.noreply.github.com>
2022-07-06 14:38:51 -0300

    Deploy teste

--- screens/Chat.tsx
+++ screens/Chat.tsx
@@ -4,10 +4,19 @@ import io from "socket.io-client";
 import styled from 'styled-components/native';
 import { RootTabScreenProps } from '../types';
 import AcoeseduComponents from '../components/AcoeseduComponents';
+import { useContext, useEffect } from 'react';
+import { InputValueContext } from '../context/InputValueContext';
 
 
 
 const Container = styled.View`
+  height: 100%;
+  border-bottom-width: 10px;
+  border-bottom-color: #b8977e;
+`
+
+
+/*const Container = styled.View`
   width: 100%;
   height: 100%;
 `
@@ -17,9 +26,13 @@ const TextInput = styled.TextInput`
   height: 50px;
   position: absolute;
   bottom: 100px;
-`
+`*/
 
-export default function Chat({ navigation }: RootTabScreenProps<'Chat'>){
+/*export default function Chat({ navigation }: RootTabScreenProps<'Chat'>)*/
+
+
+export default function Chat({ navigation }: RootTabScreenProps<'Chat'>) {
+  const {state, dispatch} = useContext(InputValueContext)
 
 
 
@@ -27,6 +40,7 @@ export default function Chat({ navigation }: RootTabScreenProps<'Chat'>){
 return (
 	<Container>
 		<AcoeseduComponents />
+    
 	</Container>
 	)
 }
