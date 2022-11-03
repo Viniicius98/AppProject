1af33318f931de9bb485292ebd75ef1a905724d4
User <User@EMERJ-DISIC5-NB.tj.rj.gov.br>
2022-11-03 14:47:03 -0300

    att

---
+++ services/apiAuthUser.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+//axios para requisição HTTP.
+
+//# No caso de API sendo acessada na mesma máquina.
+//Emulador Android Studio
+// http://10.0.2.2:3000
+
+//Emulador para iOS
+//http://localhost:3000
+
+const apiAuth = axios.create({
+  baseURL: "https://wwwh3.tjrj.jus.br/hidserverjus-api/",
+});
+
+export default apiAuth;
---
+++ services/apiGetData.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+//axios para requisição HTTP.
+
+//# No caso de API sendo acessada na mesma máquina.
+//Emulador Android Studio
+// http://10.0.2.2:3000
+
+//Emulador para iOS
+//http://localhost:3000
+
+const apiGetData = axios.create({
+  baseURL:
+    "https://wwwh3.tjrj.jus.br/HWEBAPIEVENTOS/api/magistrado/obterdados/",
+});
+
+export default apiGetData;
